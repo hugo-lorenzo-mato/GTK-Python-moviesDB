34e4522457a53394253a1f2817f4813b399d23fa
hugo-lorenzo-mato <hugo.lorenzo.mato@udc.es>
2016-10-13 17:12:40 +0200

    Internacionalización aplicación en-es

--- Main_p1.py
+++ Main_p1.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python
+#coding: utf-8 
 
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 import Controlador_p1
 
+
+# Esto permite traducir los textos escritos
+
+from os.path import abspath, dirname, join
+import gettext
+import locale
+
+APP = 'MoviesDB'
+DIR = abspath(dirname(__file__))
+LOCALE_DIR = join(DIR, 'locale')
+
+locale.setlocale(locale.LC_ALL, '')
+locale.bindtextdomain(APP, LOCALE_DIR)
+locale.textdomain(APP)
 
 Controlador_p1.Handler()
 Gtk.main()
--- Vista_p1.py
+++ Vista_p1.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+#coding: utf-8 
 
 import gi
 gi.require_version("Gtk", "3.0")
